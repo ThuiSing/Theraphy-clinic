f086931b290b8e8cf17dc3ab41031f40c4af1d44
Thui Sing <thuisingmong0@gmailcom>
2021-10-19 23:13:13 +0600

    once

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ import SingleService from "./Utilities/Pages/SingleService/SingleService";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-AOS.init();
+AOS.init({
+  once: true,
+});
 function App() {
   return (
     <div className="App">
